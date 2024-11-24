304ebae7d0fe8d8401f67bc9e6c3174b053149db
Constantin Lisnic <constantin@lisnic.dev>
2024-11-24 16:08:50 +0000

    multiplication app complete

--- Day35/ContentView.swift
+++ Day35/ContentView.swift
@@ -38,24 +38,95 @@ struct Settings: View {
     }
 }
 
-struct Question: View {
+struct Question {
     let text: String
-    let answer: Int
-
-    var body: some View {
-        Text(text)
-    }
+    let correctAnswer: Int
+    var userResponse: Int? = nil
+    var isSubmitted: Bool = false
 }
 
 struct Practice: View {
     @Binding var currentQuestion: Int
     @Binding var pack: [Question]
-    
+    @Binding var score: Int
 
     var body: some View {
         VStack {
-            pack[currentQuestion]
-            Text("=")
+            Text("Question \(currentQuestion + 1)/\(pack.count)")
+
+            Spacer()
+
+            HStack(spacing: 50) {
+                Section {
+                    Button("Previous") {
+                        currentQuestion -= 1
+                    }
+                    .disabled(currentQuestion == 0)
+
+                    VStack {
+                        Text(pack[currentQuestion].text)
+                        Text("=")
+                    }.font(.largeTitle)
+                }
+
+                Button("Next") {
+                    currentQuestion += 1
+                }
+                .disabled(currentQuestion == pack.count - 1)
+            }
+
+            Spacer()
+
+            Section {
+                Text("Your answer:")
+                HStack {
+                    TextField(
+                        "Your answer:",
+                        value: $pack[currentQuestion].userResponse,
+                        format: .number
+                    )
+                    .textFieldStyle(.roundedBorder)
+                    .foregroundStyle(.primary)
+                    .labelsHidden()
+                    .disabled(pack[currentQuestion].isSubmitted)
+                    .frame(width: 250)
+
+                    Spacer()
+
+                    if pack[currentQuestion].isSubmitted {
+                        if pack[currentQuestion].userResponse
+                            == pack[currentQuestion].correctAnswer
+                        {
+                            Text("Correct!")
+                                .foregroundStyle(.green)
+                        } else {
+                            Text("Wrong!")
+                                .foregroundStyle(.red)
+                        }
+                    }
+                }
+
+                Button("Submit") {
+                    submitAnswer()
+
+                }
+                .disabled(pack[currentQuestion].isSubmitted)
+
+            }
+            .padding()
+
+            Spacer()
+            Spacer()
+        }
+        .padding()
+    }
+
+    func submitAnswer() {
+        pack[currentQuestion].isSubmitted = true
+        if pack[currentQuestion].userResponse
+            == pack[currentQuestion].correctAnswer
+        {
+            score += 1
         }
     }
 }
@@ -65,8 +136,20 @@ struct ContentView: View {
     @State private var selectedPack = 5
     @State private var isPracticing = false
     @State private var currentQuestion: Int = 0
-
+    @State private var score: Int = 0
     @State private var pack = [Question]()
+    @State private var showFinishedAlert: Bool = false
+
+    var hasFinishedPractice: Bool {
+        if !pack.isEmpty {
+            let allQuestionsSubmitted = pack.allSatisfy {
+                $0.isSubmitted
+            }
+            return allQuestionsSubmitted
+        } else {
+            return false
+        }
+    }
 
     var body: some View {
         NavigationStack {
@@ -81,16 +164,36 @@ struct ContentView: View {
                         generateQuestionPack()
                     }
                 } else {
-                    Practice(currentQuestion: $currentQuestion, pack: $pack)
+                    Practice(
+                        currentQuestion: $currentQuestion, pack: $pack,
+                        score: $score)
                 }
 
                 Spacer()
 
                 Button("End practice") {
                     isPracticing = false
+                    currentQuestion = 0
                 }
             }
             .navigationTitle("Practice multiplication")
+            .onChange(of: hasFinishedPractice) {
+                if hasFinishedPractice {
+                    showFinishedAlert = true
+                }
+            }
+            .alert(isPresented: $showFinishedAlert) {
+                Alert(
+                    title: Text("Practice finished"),
+                    message: Text(
+                        "Your score is \(score) out of \(pack.count) questions."
+                    ),
+                    dismissButton: .default(Text("OK")) {
+                        isPracticing = false
+                        currentQuestion = 0
+                    }
+                )
+            }
         }
     }
 
@@ -99,10 +202,10 @@ struct ContentView: View {
             let random1 = Int.random(in: 2...multiplicationUpTo)
             let random2 = Int.random(in: 2...multiplicationUpTo)
             return Question(
-                text: "\(random1) x \(random2)", answer: random1 * random2)
+                text: "\(random1) x \(random2)",
+                correctAnswer: random1 * random2)
         }
     }
-
 }
 
 #Preview {
